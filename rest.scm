672bb9ef7319273bd07f665780bbe62c4d9265cb
Thomas Hintz <t@thintz.com>
2011-01-20 20:07:12 -0500

    Added html ability to sendgrid mailer

--- rest.scm
+++ rest.scm
@@ -18,15 +18,15 @@
      (mutex-unlock! *request-mutex*)
      r)))
 
-(define (send-mail #!key (subject #f) (text #f) (from #f) (from-name #f) (to #f) (reply-to #f))
-  (if (and subject text from from-name to reply-to)
+(define (send-mail #!key (subject #f) (text #f) (html #f) (from #f) (from-name #f) (to #f) (reply-to #f))
+  (if (and subject (or text html) from from-name to reply-to)
       (rest-action "https://sendgrid.com/api/mail.send.json" 'POST
                    `((api_user . ,*api-user*)
                      (api_key . ,*api-key*)
                      (subject . ,subject)
                      (to . ,to)
                      (replyto . ,reply-to)
-                     (text . ,text)
+                     ,(if html `(html . ,html) `(text . ,text))
                      (from . ,from)
                      (fromname . ,from-name)))
       (abort "You must specify all keyword parameters!")))

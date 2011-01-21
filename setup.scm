02da32b2b7e7f22d35cd7a49757e4aa9dc4ac9a0
Thomas Hintz <t@thintz.com>
2011-01-20 19:50:33 -0500

    Added rest.scm to setup.scm, cleaned up setup.scm as well.

--- setup.scm
+++ setup.scm
@@ -5,40 +5,11 @@
 ;;; end production
 
 (load "utils.scm")
-;(load "db")
-;(load "db.sdbm.scm")
 (load "mda.scm")
 (load "storage-funcs.scm")
 (load "pdf.scm")
 (load "handlers.scm")
-
-;(load "css-engine.scm")
+(load "rest.scm")
 (load "demo-data.scm")
-(load "keep-the-records.scm")
-;(load "sales")
 
-;;; virtual host mapping
-;(vhost-map `(("campnepco\\.com" .
-;              ,(lambda (continue)
-;                 (parameterize ((root-path "nepco/") (enable-ajax #t) (enable-session #t)
-;                                (prefix-path "nepco")
-;                                (valid-password?
-;                                 (lambda (user password)
-;                                   (if (user-exists? user)
-;                                       (password-matches? user password)
-;                                       #f))))
-;                   (continue))))
-;             ("troutfishingtime\\.com" .
-;              ,(lambda (continue)
-;                 (parameterize ((root-path "trout/"))
-;                   (continue))))
-;             ("localhost" . ,(lambda (continue)
-;                               (parameterize ((root-path "nepco/") (enable-ajax #t) (enable-session #t)
-;                                              (prefix-path "nepco")
-;                                              (valid-password?
-;                                               (lambda (user password)
-;                                                 (if (not (eq? (db-read "nepco" "users" user) 'not-found))
-;                                                     (password-matches? user password)
-;                                                     #f))))
-;                                 (continue))))
-;             (".*" . ,(lambda (continue) (continue)))))
+(load "keep-the-records.scm")

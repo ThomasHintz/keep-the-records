98e12404ec0c2c246eee642796e727a4fb76a49b
Thomas Hintz <t@thintz.com>
2011-01-22 04:52:59 -0500

    Added check to login, it was erroring if the sid cookie wasnt there.

--- keep-the-records.scm
+++ keep-the-records.scm
@@ -209,7 +209,7 @@
 
 (define-login-trampoline "/login-trampoline"
   hook: (lambda (user)
-          (if (session-valid? (read-cookie (session-cookie-name)))
+          (if (and (read-cookie (session-cookie-name)) (session-valid? (read-cookie (session-cookie-name))))
               (begin ($session-set! 'user user)
                      ($session-set! 'club (user-club user)))
               #f)))

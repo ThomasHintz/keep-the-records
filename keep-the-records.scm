d625325a0c99ea4f4a7974bf16f6d7272625c0d5
Thomas Hintz <t@thintz.com>
2011-04-05 16:06:21 -0400

    Fixed bug in birthdays-within causing crashes on EBCs dashboard.

--- keep-the-records.scm
+++ keep-the-records.scm
@@ -1013,7 +1013,7 @@
   title: "Dues - Club Night - KtR")
 
 (define (date->date-year d yyyy)
-  (make-date 0 0 0 0 (date-day d) (date-month d) yyyy))
+  (if d (make-date 0 0 0 0 (date-day d) (date-month d) yyyy) #f))
 
 (define (in-week? d1 d2)
   ; is d2 within the same week as d1
@@ -1025,8 +1025,8 @@
     (filter (lambda (c)
               (let* ((c-b (birthday club c))
                      (c-bd (and c-b (db->date c-b)))
-                     (c-bd-c (date->date-year c-bd (string->number (todays-yyyy)))))
-                (and c-b (date>=? c-bd-c d1) (date<=? c-bd-c d2))))
+                     (c-bd-c (and c-b (date->date-year c-bd (string->number (todays-yyyy))))))
+                (and c-bd-c (date>=? c-bd-c d1) (date<=? c-bd-c d2))))
             clubbers))
 
 (define (week-start d)

483688d7b3449b72c53d3bf5c1f3371b98ae87a7
Thomas Hintz <thintz@hotpads.com>
2012-08-28 09:44:19 -0700

    Updat sync script to use hashes only.

--- sync.scm
+++ sync.scm
@@ -7,9 +7,9 @@
 (use shell)
 
 ; dry run
-(run "rsync -naurtvv --exclude '.git' --exclude 'essays-in' --exclude 'nohup.out' --exclude 'ktr-db' --exclude '.gitignore' --exclude 'config.rb' --exclude '*~' --exclude '.sass*' --exclude 'scss' --exclude '*.scss' -e ssh . webaccess@a.keeptherecords.com:/keep-the-records | grep -v 'uptodate'")
+(run "rsync -naurtIcvv --exclude '.git' --exclude 'essays-in' --exclude 'nohup.out' --exclude 'ktr-db' --exclude '.gitignore' --exclude 'config.rb' --exclude '*~' --exclude '.sass*' --exclude 'scss' --exclude '*.scss' -e ssh . webaccess@a.keeptherecords.com:/keep-the-records | grep -v 'uptodate'")
 (display "continue? ")
 (if (string=? (read) "y")
-    (run "rsync -aurtvv --exclude '.git' --exclude 'essays-in' --exclude 'nohup.out' --exclude 'ktr-db' --exclude 'scss' --exclude '.gitignore' --exclude 'config.rb' --exclude '*~' --exclude '.sass*' --exclude '*.scss' -e ssh . webaccess@a.keeptherecords.com:/keep-the-records | grep -v 'uptodate'")
+    (run "rsync -aurcItvv --exclude '.git' --exclude 'essays-in' --exclude 'nohup.out' --exclude 'ktr-db' --exclude 'scss' --exclude '.gitignore' --exclude 'config.rb' --exclude '*~' --exclude '.sass*' --exclude '*.scss' -e ssh . webaccess@a.keeptherecords.com:/keep-the-records | grep -v 'uptodate'")
     #f)
 (exit)

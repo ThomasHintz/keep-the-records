dd2b1b73a94ed046f3dfd5fd599a17c4ae9ec2b9
Thomas Hintz <t@thinz.com>
2012-09-20 23:59:36 +0000

    Initial.

---
+++ backup-db.scm
@@ -0,0 +1,17 @@
+(use mda-client zmq posix)
+
+(define socket #f)
+
+(define (setup-socket)
+  (set! socket (make-socket 'req))
+  (connect-socket socket "tcp://localhost:4445"))
+(setup-socket)
+
+(db:pause)
+(print "paused")
+(print "begin copying")
+(process-wait (process-run "cp ~/db/ktr-db ~/"))
+(print "done copying")
+(send-message socket "unpause")
+(receive-message* socket)
+(print "unpaused")

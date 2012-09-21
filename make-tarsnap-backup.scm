d95c9b29b57a538b4033d53dcf39701ae5380244
Thomas Hintz <t@thinz.com>
2012-09-21 21:50:26 +0000

    Use fully qualified path for cron.

--- make-tarsnap-backup.scm
+++ make-tarsnap-backup.scm
@@ -2,10 +2,10 @@
 
 (change-directory "~/db")
 
-(let ((name (string-append "ktr-db-backup-" (date->string (current-date) "~m~d~y-~H~M-~S"))))
+(let ((name (string-append "ktr-db-backup-" (date->string (current-date) "~m~d~y-~H~M-~S") (if (file-exists? "~/keep-the-records/i-am-staging") "-staging" ""))))
   (process-wait (process-run (string-append "cp ~/db/ktr-db-auto-backup ~/db/" name)))
   (print "backing up " name)
-  (process-wait (process-run (string-append "tarsnap -c -f " name " "  name)))
+  (process-wait (process-run (string-append "/usr/local/bin/tarsnap -c -f " name " "  name)))
   (with-output-to-file "last-backup" (lambda () (write name)))
   (print "done backing up")
   (print "cleaning up")
--- restore-last-tarsnap-backup.scm
+++ restore-last-tarsnap-backup.scm
@@ -3,5 +3,5 @@
 (change-directory "~/db")
 
 (print "fetching")
-(process-wait (process-run (string-append "tarsnap -x -f " (with-input-from-file "last-backup" (lambda() (read))))))
+(process-wait (process-run (string-append "/usr/local/bin/tarsnap -x -f " (with-input-from-file "last-backup" (lambda() (read))))))
 (print "done")

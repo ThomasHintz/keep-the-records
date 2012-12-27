55caf359588184461b936d37627664a4f7343351
Thomas Hintz <t@thinz.com>
2012-12-27 16:52:00 +0000

    Add more comments, clean up function names.

--- src/sections/awana-structure-dsl.scm
+++ src/sections/awana-structure-dsl.scm
@@ -1,3 +1,6 @@
+; awana structure is the structure of clubs books and sections.
+; the dsl can be used to easily parse this structure
+
 (module awana-structure-dsl
   (
    awana-structure-lookup
@@ -5,17 +8,26 @@
 (import chicken scheme)
 (use srfi-1 srfi-69)
 
-; filters result of ad based on specifics given as strings
-; ex: (ad-filter (ad "TnT" 'book) '("TnT" 'book)) equals (ad "TnT" 'book)
-(define (ad-filter data-l params)
+; filters result of awana structure data based on specifics given as strings
+; ex: (as-filter (as-filter "TnT" 'book) '("TnT" 'book)) equals (as-filter "TnT" 'book)
+(define (as-filter data-l params)
   (if (> (length params) 0)
       (if (string? (first params))
-	  (ad-filter (second (first (filter (lambda (e) (string=? (first params) (first e))) data-l))) (cdr params))
+	  (as-filter (second (first (filter (lambda (e) (string=? (first params) (first e))) data-l))) (cdr params))
 	  data-l)
       data-l))
 
 (define *mem-ht* (make-hash-table))
 
+; the dsl function
+; can be used like this:
+;   ; load the structure data
+;   (define section-data (make-parameter (include "src/sections/awana-structure.data.scm")))
+;   ; look up the books for cubbies
+;   (awana-stucture-lookup section-data "Cubbies" 'book)
+;   ; returns: '("Hopper" "Jumper")
+;   ; look up the chapters and sections for cubbies
+;   (awana-struture-lookup section-data "Cubbies" "Hopper" 'chapter 'section)
 (define (awana-structure-lookup section-data . rest)
   (if (hash-table-ref/default *mem-ht* rest #f)
       (hash-table-ref *mem-ht* rest)
@@ -31,7 +43,7 @@
 				      `(,(first l)
 					,(ad-internal l (cdr params))))
 				    (second ol))))))
-		 (ad-filter (ad-internal section-data rest) rest))))
+		 (as-filter (ad-internal section-data rest) rest))))
 	(hash-table-set! *mem-ht* rest r)
 	r)))
 )

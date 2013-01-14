77dd3ae59c0660e06639c7ebc1c2a16e62ccf5e7
Thomas Hintz <t@thinz.com>
2013-01-14 14:02:30 +0000

    Update copyright/license.

--- src/db/db-interface.scm
+++ src/db/db-interface.scm
@@ -2,7 +2,7 @@
 ; email: t@thintz.com
 ; license: bsd
 
-; Copyright (c) 2012, Thomas Hintz
+; Copyright (c) 2012-2013, Thomas Hintz
 ; All rights reserved.
 
 ; Redistribution and use in source and binary forms, with or without
@@ -12,7 +12,7 @@
 ;     * Redistributions in binary form must reproduce the above copyright
 ;       notice, this list of conditions and the following disclaimer in the
 ;       documentation and/or other materials provided with the distribution.
-;     * Neither the name of the <organization> nor the
+;     * Neither the name of the author nor the
 ;       names of its contributors may be used to endorse or promote products
 ;       derived from this software without specific prior written permission.
 

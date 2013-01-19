a4e4d99f9e2292fd718b05952366fc808f1437f5
Thomas Hintz <t@thinz.com>
2013-01-19 16:00:06 +0000

    New make-tarsnap-backup works with pause db.

---
+++ src/db/make-tarsnap-backup.scm
@@ -0,0 +1,18 @@
+(use srfi-19 posix http-client)
+(load "~/keep-the-records/src/config/config-manager") (import config-manager)
+
+(change-directory "~/keep-the-records")
+
+(let ((name (string-append "ktr-db-backup-" (date->string (current-date) "~m~d~y-~H~M-~S-") (read-general-config-value 'server-id))))
+  (print "pausing db")
+  (with-input-from-request "http://localhost:12000/site/admin/db/pause" #f read-string)
+  (process-wait (process-run (string-append "cp ~/keep-the-records/ktr-db ~/keep-the-records/" name)))
+  (print "resumeing db")
+  (with-input-from-request "http://localhost:12000/site/admin/db/resume" #f read-string)
+  (print "backing up " name)
+  (process-wait (process-run (string-append "/usr/local/bin/tarsnap -c -f " name " "  name)))
+  (print "backing up last-backup")
+  (process-wait (process-run (string-append "/usr/local/bin/tarsnap -c -f " name " "  "last-backup")))
+  (print "done backing up")
+  (print "cleaning up")
+  (process-wait (process-run (string-append "rm " name))))

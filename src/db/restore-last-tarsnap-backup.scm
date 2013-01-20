703af709deb5ed774e6b46461d3250edd582b50d
Thomas Hintz <t@thinz.com>
2013-01-20 16:09:09 +0000

    Use archive named last-backup and read keyfile from config.

--- src/db/restore-last-tarsnap-backup.scm
+++ src/db/restore-last-tarsnap-backup.scm
@@ -1,7 +1,8 @@
 (use posix)
+(load "~/keep-the-records/src/config/config-manager") (import config-manager)
 
-(change-directory "~/db")
+(change-directory "~/")
 
-(print "fetching")
-(process-wait (process-run (string-append "/usr/local/bin/tarsnap -x -f " (with-input-from-file "last-backup" (lambda() (read))))))
+(print "writing to ~/")
+(process-wait (process-run (string-append "/usr/local/bin/tarsnap --keyfile " (read-general-config-value 'tarsnap-keyfile) " -x -f last-backup")))
 (print "done")
